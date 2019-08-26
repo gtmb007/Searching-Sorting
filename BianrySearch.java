faf20eb19831e85483f9456b6cff54fadcebace0
Gautam Bharadwaj <45429150+gtmb007@users.noreply.github.com>
2019-08-26 12:29:31 +0530

    Add files via upload

---
+++ BianrySearch.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Main {
+	static int round;
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter size of array: ");
+		int size=sc.nextInt();
+		int[] arr=new int[size];
+		System.out.print("Enter elements of array(in ascending order): ");
+		for(int i=0;i<size;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.print("Enter element to search: ");
+		int element=sc.nextInt();
+		int[] a=binarySearch(arr,0,size-1,element);
+		System.out.println("Index of "+element+": "+a[0]+"\nNumber of Comparisons: "+a[1]);
+	}
+	static int[] binarySearch(int[] arr,int l,int r,int element) {
+		int[] res=new int[2];
+		int mid=(l+r)/2;
+		while(l<=r) {
+			round++;
+			if(element==arr[mid]) {
+				res[0]=mid;
+				res[1]=round;	
+				return res;
+			} else if(element<arr[mid]) {
+				return binarySearch(arr,l,mid-1,element);
+			} else {
+				return binarySearch(arr,mid+1,r,element);
+			}
+		}
+		res[0]=-1;
+		res[1]=round;
+		return res;
+	}
+}
---
+++ LinearSearch.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter size of array: ");
+		int size=sc.nextInt();
+		int[] arr=new int[size];
+		System.out.print("Enter elements of array: ");
+		for(int i=0;i<size;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.print("Enter element to search: ");
+		int element=sc.nextInt();
+		System.out.println("Index of "+element+": "+linearSearch(arr,size,element));
+	}
+	static int linearSearch(int[] arr,int size,int element) {
+		int pos=-1;
+		for(int i=0;i<size;i++) {
+			if(element==arr[i]) {
+				pos=i;
+				break;
+			}
+		}
+		return pos;
+	}
+}
